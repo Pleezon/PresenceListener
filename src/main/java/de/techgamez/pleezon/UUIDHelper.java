9ac698debc4678637610c22f1c5a1ffe7424e56c
Pleezon <tomhaas0212@gmail.com>
2023-03-29 19:36:30 +0200

    Add files via upload

--- src/main/java/de/techgamez/pleezon/UUIDHelper.java
+++ src/main/java/de/techgamez/pleezon/UUIDHelper.java
@@ -8,9 +8,14 @@ import java.net.URL;
 public class UUIDHelper {
     public static String getUuid(String name) {
         try {
-            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(new URL("https://api.mojang.com/users/profiles/minecraft/"+name).openStream()));
-            String line = bufferedReader.readLine();
-            return line.split("\"id\":\"")[1].replace("\"}","").replaceAll("-","");
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(new URL("https://api.mojang.com/users/profiles/minecraft/" + name).openStream()));
+            String line;
+            while((line = bufferedReader.readLine())!= null){
+                if(line.trim().startsWith("\"id\" : \"")){
+                    return line.trim().replaceFirst("\"id\" : \"","").replaceAll("\",","");
+                }
+            }
+            return null;
         } catch (IOException e) {
             e.printStackTrace();
             return null;
@@ -19,9 +24,14 @@ public class UUIDHelper {
 
     public static String getName(String uuid) {
         try {
-            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(new URL("https://api.mojang.com/user/profiles/"+uuid.replace("-", "")+"/names").openStream()));
-            String line = bufferedReader.readLine();
-            return line.split("\"name\":\"")[1].split("\"")[0];
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(new URL("https://sessionserver.mojang.com/session/minecraft/profile/" + uuid.replace("-", "")).openStream()));
+            String line;
+            while((line = bufferedReader.readLine())!= null){
+                if(line.trim().startsWith("\"name\" : \"")){
+                    return line.trim().replaceFirst("\"name\" : \"","").replaceAll("\",","");
+                }
+            }
+            return null;
         } catch (IOException e) {
             e.printStackTrace();
             return null;
